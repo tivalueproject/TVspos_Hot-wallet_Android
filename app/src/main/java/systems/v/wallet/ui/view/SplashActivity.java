e2b8308a13572a52cf8058110df73626aa13e887
ocean <3408598@qq.com>
2019-05-28 16:13:21 +0800

    modify update

--- app/src/main/java/systems/v/wallet/ui/view/SplashActivity.java
+++ app/src/main/java/systems/v/wallet/ui/view/SplashActivity.java
@@ -3,18 +3,18 @@ package systems.v.wallet.ui.view;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
 
-import com.pgyersdk.crash.PgyCrashManager;
-import com.pgyersdk.feedback.FeedbackActivity;
-import com.pgyersdk.update.DownloadFileListener;
-import com.pgyersdk.update.PgyUpdateManager;
-import com.pgyersdk.update.UpdateManagerListener;
-import com.pgyersdk.update.javabean.AppBean;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import systems.v.wallet.R;
 import systems.v.wallet.basic.AlertDialog;
@@ -24,9 +24,12 @@ import systems.v.wallet.ui.view.main.MainActivity;
 import systems.v.wallet.ui.view.wallet.WalletInitActivity;
 import systems.v.wallet.utils.PermissionUtil;
 import systems.v.wallet.utils.ToastUtil;
+import systems.v.wallet.utils.DownloadUtil;
+import systems.v.wallet.utils.IntentUtils;
 
 public class SplashActivity extends BaseActivity {
 
+    static String  appUrl = "https://link-e-pro.oss-cn-beijing.aliyuncs.com/wallet/hot.wallet_release.apk";
     private Dialog mUpdateDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,97 +38,158 @@ public class SplashActivity extends BaseActivity {
         if (!PermissionUtil.permissionGranted(this)) {
             PermissionUtil.checkPermissions(this);
         } else {
-            FeedbackActivity.setBarImmersive(true);
-            PgyCrashManager.register();
-            new PgyUpdateManager.Builder()
-                    .setForced(false)                //设置是否强制提示更新,非自定义回调更新接口此方法有用
-                    .setUserCanRetry(false)         //失败后是否提示重新下载，非自定义下载 apk 回调此方法有用
-                    .setDeleteHistroyApk(true)     // 检查更新前是否删除本地历史 Apk， 默认为true
-                    .setUpdateManagerListener(new UpdateManagerListener() {
-                        @Override
-                        public void onNoUpdateAvailable() {
-                            //没有更新是回调此方法
-                            Log.d("pgyer", "there is no new version");
-                            launch();
-                        }
-                        @Override
-                        public void onUpdateAvailable(AppBean appBean) {
-                            //有更新回调此方法
-                            Log.d("pgyer", "there is new version can update"
-                                    + "new versionCode is " + appBean.getVersionCode());
-                            //调用以下方法，DownloadFileListener 才有效；
-                            //如果完全使用自己的下载方法，不需要设置DownloadFileListener
-                            final AppBean appBeanEx = appBean;
-                            if (mUpdateDialog == null) {
-                                mUpdateDialog = new AlertDialog.Builder(mActivity)
-                                        .setTitle(R.string.update_title)
-                                        .setMessage("")
-                                        .setPositiveButton(R.string.basic_alert_dialog_confirm, new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                PgyUpdateManager.downLoadApk(appBeanEx.getDownloadURL());
-                                                launch();
-                                            }
-                                        })
-                                        .setNegativeButton(R.string.basic_alert_dialog_cancel, new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                dialog.dismiss();
-                                                launch();
-                                            }
-                                        }).create();
-                            }
-                            mUpdateDialog.setCanceledOnTouchOutside(false);
-                            mUpdateDialog.show();
-                        }
-
-                        @Override
-                        public void checkUpdateFailed(Exception e) {
-                            //更新检测失败回调
-                            //更新拒绝（应用被下架，过期，不在安装有效期，下载次数用尽）以及无网络情况会调用此接口
-                            Log.e("pgyer", "check update failed ", e);
-                            launch();
-                        }
-                    })
-                    //注意 ：
-                    //下载方法调用 PgyUpdateManager.downLoadApk(appBean.getDownloadURL()); 此回调才有效
-                    //此方法是方便用户自己实现下载进度和状态的 UI 提供的回调
-                    //想要使用蒲公英的默认下载进度的UI则不设置此方法
-                    .setDownloadFileListener(new DownloadFileListener() {
-                        @Override
-                        public void downloadFailed() {
-                            //下载失败
-                            System.out.println("download apk failed");
-                        }
-
-                        @Override
-                        public void downloadSuccessful(Uri uri) {
-                            System.out.println("download apk success");
-                            // 使用蒲公英提供的安装方法提示用户 安装apk
-                            PgyUpdateManager.installApk(uri);
-                        }
-
-                        @Override
-                        public void onProgressUpdate(Integer... integers) {
-                            System.out.println("update download apk progress" + integers);
-                        }})
-                    .register();
-            if(!isConnectIsNomarl()){
+            final int serverVersion = GetServerAppVersion();
+            if(serverVersion > GetCurrentAppVersion()){
+                if (mUpdateDialog == null) {
+                    mUpdateDialog = new AlertDialog.Builder(mActivity)
+                            .setTitle(R.string.update_title)
+                            .setMessage("")
+                            .setPositiveButton(R.string.basic_alert_dialog_confirm, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    DownloadApp(appUrl,String.valueOf(serverVersion));
+                                    launch();
+                                }
+                            })
+                            .setNegativeButton(R.string.basic_alert_dialog_cancel, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    launch();
+                                }
+                            }).create();
+                }
+                mUpdateDialog.setCanceledOnTouchOutside(false);
+                mUpdateDialog.show();
+            }
+            else {
                 launch();
             }
         }
     }
 
-    private boolean isConnectIsNomarl() {
-        ConnectivityManager ConnectivityManager = (android.net.ConnectivityManager) mActivity
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = ConnectivityManager.getActiveNetworkInfo();
-
-        if (netInfo == null) {
-            return false;
-        } else {
-            return netInfo.isAvailable();
+    private int GetCurrentAppVersion(){
+        int versionCode = 0;
+        try {
+            PackageInfo pkg = getPackageManager().getPackageInfo(getApplication().getPackageName(), 0);
+            versionCode = pkg.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        return  versionCode;
+    }
+
+    public static String doPost(String url, String param)
+    {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        try
+        {
+            URL realUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) realUrl
+                    .openConnection();
+
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+
+            // 设置通用的请求属性
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Charset", "UTF-8");
+
+            if (param != null && !param.trim().equals(""))
+            {
+                // 获取URLConnection对象对应的输出流
+                out = new PrintWriter(conn.getOutputStream());
+                // 发送请求参数
+                out.print(param);
+                // flush输出流的缓冲
+                out.flush();
+            }
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null)
+            {
+                result += line;
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输出流、输入流
+        finally
+        {
+            try
+            {
+                if (out != null)
+                {
+                    out.close();
+                }
+                if (in != null)
+                {
+                    in.close();
+                }
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+
+    private int GetServerAppVersion(){
+        int  versionCode = 3;
+
+        return  versionCode;
+    }
+
+    public static String getCacheDir(Context context) {
+        String cachePath;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                || !Environment.isExternalStorageRemovable()) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return cachePath;
+    }
+
+    private void DownloadApp(String url,String version){
+        File file = new File(getCacheDir(mActivity), "hot.wallet_v"  + version + ".apk");
+        if (file.exists()) {
+            IntentUtils.installApk(mActivity, file);
+            return;
+        }
+        DownloadUtil.DownloadParam downloadParam = new DownloadUtil.DownloadParam(url, file);
+        new DownloadUtil(new DownloadUtil.DownloadCallBack() {
+            @Override
+            public void downLoadStart(DownloadUtil.DownloadParam param) {
+//                pb_update.setVisibility(View.VISIBLE);
+//                tv_update.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onProgressUpdate(DownloadUtil.DownloadParam param, int downloadSize, int totalSize) {
+//                pb_update.setMax(totalSize);
+//                pb_update.setProgress(downloadSize);
+            }
+
+            @Override
+            public void downloadEnd(DownloadUtil.DownloadParam param) {
+                if (param.getSaveFile().exists()) {
+                    IntentUtils.installApk(mActivity, param.getSaveFile());
+                }
+//                pb_update.setVisibility(View.GONE);
+//                tv_update.setVisibility(View.VISIBLE);
+            }
+        }).download(downloadParam);
     }
 
     @Override
