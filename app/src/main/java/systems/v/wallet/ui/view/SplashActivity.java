52fe3fa4a0a3a08140ae792793b1843d8112afa3
ocean <3408598@qq.com>
2019-05-29 13:51:46 +0800

    modify version

--- app/src/main/java/systems/v/wallet/ui/view/SplashActivity.java
+++ app/src/main/java/systems/v/wallet/ui/view/SplashActivity.java
@@ -17,8 +17,6 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
