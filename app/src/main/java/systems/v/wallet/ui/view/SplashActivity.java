814d5fe97aa92010a202f31803a016ff24346e52
ocean <3408598@qq.com>
2019-05-29 11:17:48 +0800

    http get

--- app/src/main/java/systems/v/wallet/ui/view/SplashActivity.java
+++ app/src/main/java/systems/v/wallet/ui/view/SplashActivity.java
@@ -7,7 +7,12 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -15,6 +20,9 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 
 import systems.v.wallet.R;
 import systems.v.wallet.basic.AlertDialog;
@@ -30,6 +38,7 @@ import systems.v.wallet.utils.IntentUtils;
 public class SplashActivity extends BaseActivity {
 
     static String  appUrl = "https://link-e-pro.oss-cn-beijing.aliyuncs.com/wallet/hot.wallet_release.apk";
+    static String  serverUrl = "http://47.75.180.164:8080/v1/appVsersion";
     private Dialog mUpdateDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,33 +47,60 @@ public class SplashActivity extends BaseActivity {
         if (!PermissionUtil.permissionGranted(this)) {
             PermissionUtil.checkPermissions(this);
         } else {
-            final int serverVersion = GetServerAppVersion();
-            if(serverVersion > GetCurrentAppVersion()){
-                if (mUpdateDialog == null) {
-                    mUpdateDialog = new AlertDialog.Builder(mActivity)
-                            .setTitle(R.string.update_title)
-                            .setMessage("")
-                            .setPositiveButton(R.string.basic_alert_dialog_confirm, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    DownloadApp(appUrl,String.valueOf(serverVersion));
-                                    launch();
-                                }
-                            })
-                            .setNegativeButton(R.string.basic_alert_dialog_cancel, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                    launch();
-                                }
-                            }).create();
+            final Handler handler = new Handler(){
+                @Override
+                public void handleMessage(Message msg){
+                    Bundle data = msg.getData();
+                    String val = data.getString("value");
+                    try {
+                        JSONObject object = new JSONObject(val);
+                        final  int serverVersion = object.getJSONObject("data").getInt("hotAppVersion");
+                        if(serverVersion > GetCurrentAppVersion()){
+                            if (mUpdateDialog == null) {
+                                mUpdateDialog = new AlertDialog.Builder(mActivity)
+                                        .setTitle(R.string.update_title)
+                                        .setMessage("")
+                                        .setPositiveButton(R.string.basic_alert_dialog_confirm, new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                DownloadApp(appUrl,String.valueOf(serverVersion));
+                                                launch();
+                                            }
+                                        })
+                                        .setNegativeButton(R.string.basic_alert_dialog_cancel, new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                                launch();
+                                            }
+                                        }).create();
+                            }
+                            mUpdateDialog.setCanceledOnTouchOutside(false);
+                            mUpdateDialog.show();
+                        }
+                        else {
+                            launch();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-                mUpdateDialog.setCanceledOnTouchOutside(false);
-                mUpdateDialog.show();
-            }
-            else {
-                launch();
-            }
+            };
+
+            new Thread(){
+                public void run(){
+                    String result = sendGet(serverUrl,"");
+                    Message msg = Message.obtain();
+                    Bundle data = new Bundle();
+                    data.putString("value",result);
+                    msg.setData(data);
+                    if (result.isEmpty()){
+                        launch();
+                    }else {
+                        handler.sendMessage(msg);
+                    }
+                }
+            }.start();
         }
     }
 
@@ -80,74 +116,51 @@ public class SplashActivity extends BaseActivity {
         return  versionCode;
     }
 
-    public static String doPost(String url, String param)
+    public static String sendGet(String url, String param)
     {
-        PrintWriter out = null;
-        BufferedReader in = null;
         String result = "";
-        try
-        {
-            URL realUrl = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) realUrl
-                    .openConnection();
-
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            conn.setUseCaches(false);
-            conn.setRequestMethod("POST");
-
+        BufferedReader in = null;
+        try {
+            String urlNameString = url + "?" + param;
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
             // 设置通用的请求属性
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("Charset", "UTF-8");
-
-            if (param != null && !param.trim().equals(""))
-            {
-                // 获取URLConnection对象对应的输出流
-                out = new PrintWriter(conn.getOutputStream());
-                // 发送请求参数
-                out.print(param);
-                // flush输出流的缓冲
-                out.flush();
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setConnectTimeout(3000);
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
             }
-            // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
             String line;
-            while ((line = in.readLine()) != null)
-            {
+            while ((line = in.readLine()) != null) {
                 result += line;
             }
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
             e.printStackTrace();
         }
-        // 使用finally块来关闭输出流、输入流
-        finally
-        {
-            try
-            {
-                if (out != null)
-                {
-                    out.close();
-                }
-                if (in != null)
-                {
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
                     in.close();
                 }
-            } catch (Exception ex)
-            {
-                ex.printStackTrace();
+            } catch (Exception e2) {
+                e2.printStackTrace();
             }
         }
         return result;
-    }
-
-
-    private int GetServerAppVersion(){
-        int  versionCode = 3;
-
-        return  versionCode;
     }
 
     public static String getCacheDir(Context context) {
